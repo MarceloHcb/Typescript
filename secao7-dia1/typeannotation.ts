60b26ded42db293c5c078ed616709a4d5f29d9bd
MarceloHcb <marcelohcb@msn.com>
2023-05-21 11:10:04 -0300

    up

--- secao7-dia1/typeannotation.ts
+++ secao7-dia1/typeannotation.ts
@@ -16,10 +16,10 @@ console.log(getFavoriteNumber())
 let sum = (a, b) => a + b
 sum(1, '2')
 
-function printPersonalInfo(data: { name: string; birthYear: number }) {
+function printPersonalInfo2(data: { name: string; birthYear: number }) {
     console.log(`${data.name} was born in ${data.birthYear}.`);
 }
-printPersonalInfo({ name: 'Rogerinho', birthYear: 1978 });
+printPersonalInfo2({ name: 'Rogerinho', birthYear: 1978 });
 
 const evenNumbers: number[] = [2, 4, 6];
 const vowel: string[] = ['a', 'e', 'i', 'o', 'u'];
@@ -28,3 +28,54 @@ const booleanValues: boolean[] = [true, false];
 evenNumbers.push(8) // Funciona
 evenNumbers.push('8') // Erro
 
+
+let role:any = 10
+
+role =  'kdfnlg'
+
+role = []
+role = {}
+role.toUpperCase()
+// if(typeof role === 'string'){
+//     role.toUpperCase()
+// }
+
+let ages: unknown = 10
+ages = 'jksdf'
+ages = true
+ages = []
+
+if(typeof ages === 'string'){
+    ages.toUpperCase()
+}
+// ages.toUpperCase()
+
+const greets = (name:string):string => {
+    console.log('hello', name);
+    return name
+    
+}
+
+console.log(greets('mclo'));
+
+type User = {
+    name: string,
+    age: number,
+    // address?: string | undefined
+    address?: string 
+}
+// interrogação não funciona fora do objeto
+let address: number | null = null
+
+const user: User = {
+    name: 'kjhk',
+    age: 2,
+}
+
+let gr: string = ''
+let abc : string | undefined = 'undefined'
+function test(a:string) {
+    a.toLowerCase()
+}
+
+test(abc)
